f97567cd1d2a431c882579b1bf4dbbb43512c869
Aleksandr <alexander.ivanov.35@gmail.com>
2020-06-01 12:28:57 +0900

    host env

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const host = 'localhost:8808'
+const IP_VLAN_KEEPER_HOST_ADDRESS = process.env.IP_VLAN_KEEPER_HOST_ADDRESS || '10.0.2.1';
+
+const host = IP_VLAN_KEEPER_HOST_ADDRESS + ':8808'
 
 export default axios.create({
   baseURL: 'http://' + host,
