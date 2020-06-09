8bb333e2ba3cebfe59b36f0b90fcdc267ce66636
Aleksandr <alexander.ivanov.35@gmail.com>
2020-06-09 10:00:31 +0900

    env fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const IP_VLAN_KEEPER_HOST_ADDRESS = process.env.IP_VLAN_KEEPER_HOST_ADDRESS || '10.0.2.1';
+const IP_VLAN_KEEPER_HOST_ADDRESS = process.env.REACT_APP_IP_VLAN_KEEPER_HOST_ADDRESS || '10.0.2.1';
 
 const host = IP_VLAN_KEEPER_HOST_ADDRESS + ':8808'
 
